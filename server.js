ca7b58313c5483172de7d8b63c3512c252ac83d5
Krystian Derdziński <derdzinski.krystian@gmail.com>
2022-01-27 23:43:43 +0100

    Checking response status in server.js

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ app.set('view engine', 'ejs');
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
-	res.render("index")
+	if (res.statusCode === 200) {
+		res.render("index")
+	} else {
+		console.log('something went wrong...')
+	}
 });
 
 app.listen(5000);
