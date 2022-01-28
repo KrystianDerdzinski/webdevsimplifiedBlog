a5e04edcf30283a74483860f562aee68de83e4d6
Krystian Derdziński <derdzinski.krystian@gmail.com>
2022-01-29 00:21:21 +0100

    Added Bootstrap

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.use('/articles', articleRouter);
 
 app.get('/', (req, res) => {
 	if (res.statusCode === 200) {
-		res.render("index")
+		const articles = [{
+			title: 'Test article',
+			createdAt: Date.now(),
+			description: 'Test description'
+		}]
+		res.render("index", { articles: 'Hello' })
 	} else {
 		console.log('something went wrong...')
 	}
