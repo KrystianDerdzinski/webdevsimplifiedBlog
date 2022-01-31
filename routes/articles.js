c9073659b94c72335d22df2fc56e9b4d49ffea95
Krystian Derdziński <derdzinski.krystian@gmail.com>
2022-01-31 19:50:59 +0100

    Adding new article

--- routes/articles.js
+++ routes/articles.js
@@ -3,15 +3,15 @@ const Article = require('./../models/article');
 const router = express.Router();
 
 router.get('/new', (req, res) => {
-	res.render('articles/new', { article: new Article})
+	res.render('articles/new', { article: new Article()})
 })
 
 router.get('/:id', (req, res) => {
-
+	res.send(req.params.id);
 })
 
 router.post('/', async (req, res) => {
-	const article = new Article({
+	let article = new Article({
 		title: req.body.title,
 		description: req.body.description,
 		markdown: req.body.markdown
@@ -20,6 +20,7 @@ router.post('/', async (req, res) => {
 		article = await article.save()
 		res.redirect(`/articles/${article.id}`)
 	} catch (e) {
+		console.log(e)
 		res.render('articles/new', { article : article })
 	}
 })
